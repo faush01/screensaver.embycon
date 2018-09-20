78c90f5360a65b43527ff6a54c3cdadd36b832af
shaun <shaun@bluebit.com.au>
2018-09-20 13:17:50 +1000

    first cut

---
+++ default.py
@@ -0,0 +1,57 @@
+import sys
+import xbmcaddon
+import xbmcgui
+import xbmc
+
+Addon = xbmcaddon.Addon('screensaver.embycon')
+
+__scriptname__ = Addon.getAddonInfo('name')
+__path__ = Addon.getAddonInfo('path')
+
+
+class Screensaver(xbmcgui.WindowXMLDialog):
+    class ExitMonitor(xbmc.Monitor):
+
+        def __init__(self, exit_callback):
+            self.exit_callback = exit_callback
+
+        def onScreensaverDeactivated(self):
+            xbmc.log('EmbyCon.Screensaver: sending exit_callback')
+            self.exit_callback()
+
+    def onInit(self):
+        xbmc.log('EmbyCon.Screensaver: onInit')
+        self.monitor = self.ExitMonitor(self.exit)
+
+    def exit(self):
+        xbmc.log('EmbyCon.Screensaver: Exit requested')
+
+        #xbmc.executebuiltin("RunScript(plugin.video.embycon,0,?mode=CHANGE_USER)")
+
+        screen_saver_image = self.getControl(3000)
+        screen_saver_image.setVisible(False)
+
+        screen_saver_label = self.getControl(3001)
+        screen_saver_label.setLabel("Closing...")
+
+        loops = 0
+        while not xbmc.getCondVisibility("Window.IsVisible(selectdialog)") and loops < 20:
+            loops = loops + 1
+            xbmc.log("EmbyCon.Screensaver, waiting 500 ms for selectdialog : " + str(loops))
+            xbmc.sleep(500)
+
+        if xbmc.getCondVisibility("Window.IsVisible(selectdialog)"):
+            xbmc.log("EmbyCon.Screensaver selectdialog is visible")
+        else:
+            xbmc.log("EmbyCon.Screensaver selectdialog did not show")
+
+        self.close()
+
+
+if __name__ == '__main__':
+    xbmc.log('EmbyCon.Screensaver Started')
+    screensaver_gui = Screensaver('screen_saver.xml', __path__, 'default')
+    screensaver_gui.doModal()
+    xbmc.log('EmbyCon.Screensaver Exited')
+    del screensaver_gui
+    sys.modules.clear()
